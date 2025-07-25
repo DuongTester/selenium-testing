102f675cbae31c3fd1c25e8b3e7d36a6710e073a
PhanTTDuong <21130331@st.hcmuaf.edu.vn>
2025-07-25 17:58:23 +0700

    checkValue

--- src/test/java/com/duongphan/selenium/Topic5Xpath.java
+++ src/test/java/com/duongphan/selenium/Topic5Xpath.java
@@ -11,6 +11,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+
 public class Topic5Xpath {
     WebDriver driver;
   @BeforeClass
@@ -42,10 +44,18 @@ public class Topic5Xpath {
  public void TC02_RegisterWithInvalidEmail() {
 	  driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 	  driver.findElement(By.id("txtFirstname")).sendKeys("DuongZuiZe");
+    driver.findElement(By.id("txtEmail")).sendKeys("D@uong@gmail1x_z2.com");
+	  driver.findElement(By.id("txtCEmail")).sendKeys("Duonqqg@gmail.com");
 	  driver.findElement(By.id("txtPassword")).sendKeys("1234");
 	  driver.findElement(By.id("txtCPassword")).sendKeys("1234");
 	  driver.findElement(By.id("txtPhone")).sendKeys("09765876234");
 	  driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    String actualEmailError = driver.findElement(By.id("txtEmail-error")).getText();
+    Assert.assertEquals(actualEmailError, "Vui lòng nhập email hợp lệ");
+    
+    String actualConfirmEmailError = driver.findElement(By.id("txtCEmail-error")).getText();
+    Assert.assertEquals(actualConfirmEmailError, "Email nhập lại không đúng");
 	
  }
  
@@ -53,15 +63,20 @@ public class Topic5Xpath {
   public void TC_03_RegisterWithConfirmEmail() {
 	  driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 	  driver.findElement(By.name("txtFirstname")).sendKeys("DuongZuiZe");
-	  driver.findElement(By.id("txtCEmail")).sendKeys("Duong@gmail.com");
+    driver.findElement(By.id("txtEmail")).sendKeys("Duong@gmail.com");
+	  driver.findElement(By.id("txtCEmail")).sendKeys("Duonqqg@gmail.com");
 	  driver.findElement(By.name("txtPassword")).sendKeys("1234");
 	  driver.findElement(By.name("txtCPassword")).sendKeys("1234");
 	  driver.findElement(By.name("txtPhone")).sendKeys("09765876234");
 	  driver.findElement(By.xpath("//div[@class='field_btn']//button[@type='submit']")).click();
+
+    String actualEmailConfirmErro = driver.findElement(By.id("txtCEmail-error")).getText();
+    Assert.assertEquals(actualEmailConfirmErro, "Email nhập lại không đúng");
   }
  
 @Test
 public void TC_04_RegisterWithIncorrectConfirmPass() {
+   driver.get("https://alada.vn/tai-khoan/dang-ky.html");
    driver.findElement(By.id("txtFirstname")).sendKeys("DuongZuiZe");
     driver.findElement(By.id("txtEmail")).sendKeys("Duong@gmail.com");
     driver.findElement(By.name("txtCEmail")).sendKeys("Duong@gmail.com");
@@ -69,9 +84,13 @@ public void TC_04_RegisterWithIncorrectConfirmPass() {
     driver.findElement(By.name("txtCPassword")).sendKeys("123456789");
     driver.findElement(By.name("txtPhone")).sendKeys("09765876234");
     driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    String actualConfirmPassError = driver.findElement(By.id("txtCPassword-error")).getText();
+    Assert.assertEquals(actualConfirmPassError, "Mật khẩu bạn nhập không khớp");
 }
 @Test
 public void TC_05_RegisterWithInvalidPhone() {
+   driver.get("https://alada.vn/tai-khoan/dang-ky.html");
    driver.findElement(By.id("txtFirstname")).sendKeys("DuongZuiZe");
     driver.findElement(By.id("txtEmail")).sendKeys("Duong@gmail.com");
     driver.findElement(By.name("txtCEmail")).sendKeys("Duong@gmail.com");
@@ -79,9 +98,13 @@ public void TC_05_RegisterWithInvalidPhone() {
     driver.findElement(By.name("txtCPassword")).sendKeys("123456789");
     driver.findElement(By.name("txtPhone")).sendKeys("097658w132e76234");
     driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    String actualPhoneError = driver.findElement(By.id("txtPhone-error")).getText();
+    Assert.assertEquals(actualPhoneError, "Vui lòng nhập con số");
 }
 @Test
 public void TC_06_RegisterWithIncorrectPass() {
+   driver.get("https://alada.vn/tai-khoan/dang-ky.html");
    driver.findElement(By.id("txtFirstname")).sendKeys("DuongZuiZe");
     driver.findElement(By.id("txtEmail")).sendKeys("Duong@gmail.com");
     driver.findElement(By.name("txtCEmail")).sendKeys("Duong@gmail.com");
@@ -89,6 +112,13 @@ public void TC_06_RegisterWithIncorrectPass() {
     driver.findElement(By.name("txtCPassword")).sendKeys("19111");
     driver.findElement(By.name("txtPhone")).sendKeys("09765876234");
     driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    String actualPassError = driver.findElement(By.id("txtPassword-error")).getText();
+    String actualConfirmPassError = driver.findElement(By.id("txtCPassword-error")).getText();
+
+    // Kiểm tra nội dung thông báo lỗi
+    Assert.assertEquals(actualPassError, "Mật khẩu phải có ít nhất 6 ký tự");
+    Assert.assertEquals(actualConfirmPassError, "Mật khẩu phải có ít nhất 6 ký tự");
 }
   @AfterTest
   public void afterTest() {
